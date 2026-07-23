3dd21bd8b07198e5b61bc5aceb570fe94c441b00
sultani <mohammadhamedsultani01@gamil.com>
2026-07-23 07:23:36 +0430

    Todo app

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './Todo App/ToDo'
+import App from './Todo App/new'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 
